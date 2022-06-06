4496ce9c4e251a5fc35a1934a1ea6a60fd9bb435
Rafael Patricio <rafaelpatricio91@gmail.com>
2022-06-06 20:01:35 -0300

    add comentários

--- src/com/rafa/lerTxt/LeitorTxt.java
+++ src/com/rafa/lerTxt/LeitorTxt.java
@@ -7,17 +7,24 @@ import java.util.Scanner;
 
 public class LeitorTxt {
 	
-	public List<String> getTxtContent(String directoryLocation) throws Exception {
-		String cpf = "";
+	public List<String> getTxtContent(String directoryLocation) throws Exception { //recebe o diretorio como parametro
+		//instancia as variaveis necessarias
+		tring cpf = "";
 		List<String> listaCpfs = new ArrayList<>();
 		Scanner scanner = null;
+		//monta o arquivo
 		File arquivo = new File(directoryLocation);
 		
 		try {
+			//itera as linhas do arquivo
 			scanner = new Scanner(arquivo);
+			//enquanto tiver linha preenchida
 			while (scanner.hasNextLine()) {
+				//pega e joga na variavel cpf
 				cpf = scanner.nextLine();
+				//se a linha tinha alguma informação
 				if (cpf.length() > 0) {
+					//joga dentro da lista
 					listaCpfs.add(cpf);
 				}
 			}
@@ -25,10 +32,12 @@ public class LeitorTxt {
 			throw new Exception("Erro - ",e);
 		}
 		finally {
+			//serve so pra fechar o scanner
 			if (scanner != null) {
 				scanner.close();
 			}
 		}
+		//retorna a lista de cpfs
 		return listaCpfs;
 	}
 
--- src/com/rafa/lerTxt/Teste.java
+++ src/com/rafa/lerTxt/Teste.java
@@ -2,9 +2,13 @@ package com.rafa.lerTxt;
 
 public class Teste {
 	public static void main(String[] args) throws Exception {
+		//Instancia a classe que tem o método pra ler o txt
 		LeitorTxt leitorTxt = new LeitorTxt();
 		try {
+			//itera em cima da lista que será retornada pelo método. Passar o dir como parametro.
 			for (String cpf : leitorTxt.getTxtContent("/home/rafa/teste/testea.txt")) {
+				//escreve na tela a linha. aqui será onde vc vai fazer todas as manipulações necessárias
+				//com os dados
 				System.out.println(cpf);
 			}
 		} catch (Exception e) {
